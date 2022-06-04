096c2e2100f71539597a05a10cc85889081f364d
Your Name <you@example.com>
2022-06-04 17:00:28 -0300

    template_engine_blade

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<h1> Hello World <h1>
+    <a href="/"> Ir para home</a>
+    <a href="/products">PRodutos</a>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Matheus";
+    $idade = 29;
+    $arr = [10,20,30,40,50];
+    $nomes = ["Matheus", "Maria", "João", "Saulo"];
+
+    return view('welcome', 
+        [
+            'nome' => $nome, 
+            'idade' => $idade,
+            'arr' => $arr,
+            'nomes' => $nomes,
+        ]);
+    
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/produtos', function () {
+    return view('products');
 });
