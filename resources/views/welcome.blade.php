f66f6b4e58071ecd88e2e6baa3bc9a43bdfa24cb
Your Name <you@example.com>
2022-06-04 17:35:43 -0300

    Layout_blade_css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Olá mundo!</title>
+@section('title', 'HDC Events')
 
-        <link rel="stylesheet" href="/css/styles.css">
-        <script src="/js/scripts.js"></script>
-    </head>
-    <body>
+@section('content')
+
         <h1>Algum título</h1>
-        <img src="/img/banner.jpg" alt="banner">
         @if(10 > 5)
             <p> A condição é true </p>
 
@@ -40,5 +33,5 @@
             <p>{{ $loop->index }}</p>
             <p>{{ $nome }}</p>
         @endforeach
-    </body>
-</html>
+        
+@endsection
