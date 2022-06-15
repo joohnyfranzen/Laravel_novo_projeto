5879743f3e5b6c753869cf20bdafea05ca95dc9e
Your Name <you@example.com>
2022-06-15 15:09:53 -0300

    dashboard Routes

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,4 @@ Route::get('/contatos', function () {
     return view('contact');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
